d7de65da6f963d076856647d155a84de514baf58
Frontend-kik <153603429+Frontend-kik@users.noreply.github.com>
2025-03-08 09:48:50 +0200

    todo list

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import styles from './app.module.css';
+
+export const App = () => {
+	const [task, setTask] = useState([]); // Держим список задач в состоянии
+
+	// const onClickHandler = () => {
+	// 	const newTask = [...task, { id: Date.now(), title: 'Задача' }];
+	// 	setTask(newTask);
+	// };
+	// Загружаем список задач с сервера с JSON Placeholder при первом рендере
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+			.then((response) => response.json())
+			.then((data) => setTask(data))
+			.catch((error) => console.log('Ошибка загрузки данных с сервера:', error));
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h2>Спискок задач:</h2>
+				{/* <button>Добавить задачу</button> */}
+				<ul className={styles.item}>
+					{task.map((task) => (
+						<li key={task.title}>
+							<input type="checkbox" defaultChecked={task.completed} />
+							<span>{task.title}</span>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
