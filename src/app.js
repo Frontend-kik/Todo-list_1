40064d177013074fbfd14b95708016294d20ab23
Frontend-kik <153603429+Frontend-kik@users.noreply.github.com>
2025-03-11 17:20:12 +0200

    replacing JSON Placeholder with JSON Server

--- src/app.js
+++ src/app.js
@@ -2,30 +2,97 @@ import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
-	const [task, setTask] = useState([]); // Держим список задач в состоянии
+	const [task, setTask] = useState([]); //  список задач в состоянии
+	const [loading, setLoading] = useState(false); //  состояние загрузки данных
 
-	// const onClickHandler = () => {
-	// 	const newTask = [...task, { id: Date.now(), title: 'Задача' }];
-	// 	setTask(newTask);
-	// };
-	// Загружаем список задач с сервера с JSON Placeholder при первом рендере
+	const [newTask, setNewTask] = useState(''); // Новая задача
+
+	// Загружаем данные с сервера
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+		setLoading(true);
+		fetch('http://localhost:3000/tasks')
 			.then((response) => response.json())
 			.then((data) => setTask(data))
-			.catch((error) => console.log('Ошибка загрузки данных с сервера:', error));
+			.catch((error) => console.log('Ошибка загрузки данных:', error))
+			.finally(() => setLoading(false)); // После загрузки данных устанавливаем состояние загрузки в false
 	}, []);
+
+	// Добавление задачи
+	const addTask = () => {
+		if (!newTask.trim() === '') return; // Проверка на пустую строку
+
+		const taskObj = { id: Date.now(), title: newTask, completed: false };
+
+		fetch('http://localhost:3000/tasks', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json: charset=utf-8' },
+			body: JSON.stringify({ taskObj }),
+		})
+			.then(() => setTask([...task, taskObj])) // Обновляем список
+			.then((error) => console.log('Ошибка загрузки:', error))
+			.finally(() => setLoading(false));
+
+		setNewTask(''); // Очищаем поле ввода
+	};
+
+	//	Изменение статуса задачи(выполнена/не выполнена)
+	const toggleTask = (id) => {
+		const updateTasks = task.map((task) =>
+			task.id === id ? { ...task, completed: !task.completed } : task,
+		);
+		setTask(updateTasks); // Обновляем состояние
+
+		const updateTask = task.find((task) => task.id === id); // Находим задачу по id
+		fetch(`http://localhost:3000/tasks/${id}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json: charset=utf-8' },
+			body: JSON.stringify(updateTask),
+		}).catch((error) => console.log('Ошибка загрузки:', error));
+	};
+
+	// Удаление задачи
+	const deleteTask = (id) => {
+		const updateTasks = task.filter((task) => task.id !== id);
+		setTask(updateTasks); // Обновляем состояние
+
+		fetch(`http://localhost:3000/tasks/${id}`, {
+			method: 'DELETE',
+		}).catch((error) => console.log('Ошибка загрузки:', error));
+	};
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
 				<h2>Спискок задач:</h2>
-				{/* <button>Добавить задачу</button> */}
+
+				{/* Поле ввода для новой задачи */}
+				<input
+					type="text"
+					value={newTask}
+					onChange={(e) => setNewTask(e.target.value)}
+					placeholder="Добавить задачу..."
+				/>
+				<button className={styles.btn} onClick={addTask}>
+					Добавить
+				</button>
+				{loading ? <p>Загрузка...</p> : null}
+
+				{/* Список задач */}
 				<ul className={styles.item}>
 					{task.map((task) => (
-						<li key={task.title}>
-							<input type="checkbox" defaultChecked={task.completed} />
+						<li key={task.id}>
 							<span>{task.title}</span>
+							<input
+								type="checkbox"
+								checked={task.completed}
+								onChange={() => toggleTask(task.id)}
+							/>
+							<button
+								className={styles.btn}
+								onClick={deleteTask.bind(null, task.id)}
+							>
+								Удалить
+							</button>
 						</li>
 					))}
 				</ul>
